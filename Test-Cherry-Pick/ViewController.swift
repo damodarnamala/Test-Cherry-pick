b57f648141bb08dc0aa78abe278f6f362adb7ab3
Damodar <damodar@backbase.com>
2025-01-16 00:54:04 +0530

    added struct

--- Test-Cherry-Pick/ViewController.swift
+++ Test-Cherry-Pick/ViewController.swift
@@ -7,41 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
-    /*
-     Step 0: Follow the instructions from Amazon to create a Login with Amazon (LWA) account and Security Profile. The most important part is the API key for iOS. Modify “info.plist” to add a key “APIKey” with the API key value, and add “URL types” with identifier equals to the app Bundle Identifier while “URL Schemes” equals to “amzn-[BUNDLE_ID]” .
-     
-     */
-    
-    
-    /*
-    Step 0: Follow the instructions from Amazon to create a Login with Amazon (LWA) account and Security Profile. The most important part is the API key for iOS. Modify “info.plist” to add a key “APIKey” with the API key value, and add “URL types” with identifier equals to the app Bundle Identifier while “URL Schemes” equals to “amzn-[BUNDLE_ID]” .
-
-     */
-
-    
-    /*
-    Step 0: Follow the instructions from Amazon to create a Login with Amazon (LWA) account and Security Profile. The most important part is the API key for iOS. Modify “info.plist” to add a key “APIKey” with the API key value, and add “URL types” with identifier equals to the app Bundle Identifier while “URL Schemes” equals to “amzn-[BUNDLE_ID]” .
-
-     */
-
-    
-    /*
-    Step 0: Follow the instructions from Amazon to create a Login with Amazon (LWA) account and Security Profile. The most important part is the API key for iOS. Modify “info.plist” to add a key “APIKey” with the API key value, and add “URL types” with identifier equals to the app Bundle Identifier while “URL Schemes” equals to “amzn-[BUNDLE_ID]” .
-
-     */
-
-    
-    /*
-    Step 0: Follow the instructions from Amazon to create a Login with Amazon (LWA) account and Security Profile. The most important part is the API key for iOS. Modify “info.plist” to add a key “APIKey” with the API key value, and add “URL types” with identifier equals to the app Bundle Identifier while “URL Schemes” equals to “amzn-[BUNDLE_ID]” .
-
-     */
-
+struct App {
+    var name: String
 }
 
